4a22920829c5a3e7881d69ec9c4f1d2d15ee5037
Nikkabest <48639648+artemru123@users.noreply.github.com>
2024-07-21 18:02:46 +0700

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,14 +1,10 @@
-<?php
-$recaptcha_secret = "6LceDhUqAAAAABV9Hbfp_IktcARnT_GDAcEEVKgu";
-$recaptcha_response = $_POST['g-recaptcha-response'];
-$remoteip = $_SERVER['REMOTE_ADDR'];
-
-$response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$recaptcha_secret&response=$recaptcha_response&remoteip=$remoteip");
-$responseKeys = json_decode($response, true);
-
-if(intval($responseKeys["success"]) !== 1) {
-    echo "reCAPTCHA verification failed. Please try again.";
+$secret = "6LceDhUqAAAAABV9Hbfp_IktcARnT_GDAcEEVKgu";
+$response = $_POST['g-recaptcha-response'];
+$verify = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$response");
+$captcha_success = json_decode($verify);
+if ($captcha_success->success == true) {
+    echo "Капча прошла успешно. Действие можно продолжать.";
 } else {
-    echo "reCAPTCHA verification successful. Your form was submitted.";
+    echo "Капча не прошла. Попробуйте еще раз.";
 }
 ?>
