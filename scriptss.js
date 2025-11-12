053b85840d22a8d73237ddf2a4ed96cc73e08348
Nikkabest <48639648+artemru123@users.noreply.github.com>
2025-11-12 21:40:02 +0700

    Add files via upload

--- scriptss.js
+++ scriptss.js
@@ -1,12 +1,12 @@
-function get(skuns) {
-    document.getElementById("confurm").style.display = "flex";
-    document.getElementById("confurms").onclick = function() {getss(skuns);};
+const datas = [];function get(skuns) {
+    document.getElementById("confurm").style.animation = 'fadeIn 0.2s forwards';setTimeout(() => {document.getElementById("confurm").style.display = "flex";}, 100);
+    document.getElementById("button").onclick = function() {getss(skuns);};
 
 }
-function getss(skuns) {
+function getss(skuns) { const tg = window.Telegram.WebApp;
     
     
-    tg.sendData("buyss_" + skuns)
+     const result = datas.find(items => items.qu === skuns); tg.sendData(JSON.stringify(result));
 }
 const urlParams = new URLSearchParams(window.location.search);
 
@@ -17,23 +17,23 @@ const urlParams = new URLSearchParams(window.location.search);
 
 
 
-function get_ss(){
+function skuns(){
 const balance = parseFloat(urlParams.get('bal'));
 if (1 > 2) {
     console.error('Некорректный баланс');
     
 } else {
-    fetch('https://market.csgo.com/api/v2/prices/RUB.json')
+    fetch('https://yolo-bot.vercel.app/api/proxy?url=' + encodeURIComponent('https://market.csgo.com/api/v2/prices/RUB.json'))
     
     
         .then(response => response.json())
-        .then(data => {
+        .then(datass => {
         
-            const lists = data.items; 
-            const datas = []; 
+            const lists = datass.items; 
+            
         
-            let qu = 0; 
-            lists.forEach(infos => {
+            let qu = 0; background = document.getElementById("skunss");
+            lists.forEach((infos, index) => { const price = parseFloat(infos.price); if (price > (balance / 100 * 70) && (balance / 100 * 90) > price) { const hashs = infos.market_hash_name; datas.push({ price, hashs, qu }); let utems = document.createElement("div"); utems.classList.add("container-skunss", "games_indexs-items"); let png = document.createElement("img"); png.src = "https://cdn2.csgo.com/item/image/width=500/" + encodeURIComponent(hashs) + ".png"; png.classList.add("skuns"); utems.onclick = ((quCopy) => () => get(quCopy))(qu); utems.append(png);background.append(utems); qu++; }});
                 
                 
                 
@@ -48,16 +48,16 @@ if (1 > 2) {
                 
                 
                 
-                const price = parseFloat(infos.price); 
-                if (price > (balance / 100 * 70) - (pages * 3) && (balance / 100 * 90) - (pages * 3) > price) {
-                    const hashs = infos.market_hash_name; 
-
-                    datas.push({ price, hashs, qu });
-                }
+               
+                
+                    
+                    
+                    
                 
                 
-            });
-            console.log(datas);
+                
+            
+            
         })
         .catch(error => {
             console.error('Ошибка при запросе данных:', error);
@@ -90,3 +90,6 @@ if (1 > 2) {
         
         });
 }}
+
+
+document.addEventListener('DOMContentLoaded', () => {skuns();window.onload = () => { const loadingScreen = document.getElementById('loading-screen'); loadingScreen.style.animation = 'fadeIn 1s forwards reverse'; setTimeout(() => loadingScreen.style.display = 'none', 1000); document.getElementById('confurm').addEventListener('click', e => { if (e.target.id === 'confurm') { document.getElementById("confurm").style.animation = 'fadeOut 0.2s forwards'; setTimeout(() => { document.getElementById("confurm").style.display = "none"; }, 100); } }); }; });
